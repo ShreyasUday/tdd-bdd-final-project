dbf78b3576eabba5ff7e6c9231e3432fdde6715c
ShreyasUday <udayshreyas123456@gmail.com>
2026-06-18 18:14:08 -0400

    Complete Task 1: Update ProductFactory

--- tests/factories.py
+++ tests/factories.py
@@ -17,18 +17,18 @@
 """
 Test Factory to make fake objects for testing
 """
+# tests/factories.py
 import factory
-from factory.fuzzy import FuzzyChoice, FuzzyDecimal
+from factory.fuzzy import FuzzyChoice, FuzzyFloat
 from service.models import Product, Category
 
-
 class ProductFactory(factory.Factory):
-    """Creates fake products for testing"""
-
     class Meta:
-        """Maps factory to data model"""
-
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = factory.Faker("word")
+    description = factory.Faker("text")
+    price = FuzzyFloat(0.5, 2000.0)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(choices=[c for c in Category])
